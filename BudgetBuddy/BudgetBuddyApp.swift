46d0d0c149e9ca6d7b54c65c37bf2be49868f7cb
Basim415 <bshahzad@horizon.csueastbay.edu>
2025-05-19 01:20:37 -0700

    Initial Commit

---
+++ BudgetBuddy/BudgetBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetBuddyApp.swift
+//  BudgetBuddy
+//
+//  Created by Basim Shahzad on 5/19/25.
+//
+
+import SwiftUI
+
+@main
+struct BudgetBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BudgetBuddyTests/BudgetBuddyTests.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetBuddyTests.swift
+//  BudgetBuddyTests
+//
+//  Created by Basim Shahzad on 5/19/25.
+//
+
+import Testing
+@testable import BudgetBuddy
+
+struct BudgetBuddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
